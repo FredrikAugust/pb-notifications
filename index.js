507a7cd2dd5e6abd117ddeb90cd0b28e0bcc05e1
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2018-02-21 17:32:51 +0100

    Finish setting up notificatin service
    libnotify does not support custom images, so a default phone icon will be used

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 /**
  *
  * Main file; send a notification when you get a notification on pushbullet.
@@ -6,7 +7,8 @@
  *
  */
 
-const WebSocket = require('websocket');
+const WebSocket = require('ws');
+const notifier = require('node-notifier');
 
 const API_KEY = process.env.PUSHBULLET_API_KEY;
 
@@ -16,6 +18,27 @@ conn.on('open', () => {
   console.log('Connection established.');
 });
 
-conn.on('message', (data) => {
-  console.log(`Received data: ${data}`);
+conn.on('message', (raw) => {
+  const data = JSON.parse(raw);
+
+  console.log(`Received message of type ${data.type}`);
+
+  if (data["type"] != 'push') {
+    return;
+  }
+
+  console.log(`Dispatching notification: ${data.push.application_name} | ${data.push.title}`);
+
+  notifier.notify({
+    title: `${data.push.application_name} | ${data.push.title}`,
+    message: data.push.body,
+    icon: 'phone'
+  });
+});
+
+conn.on('close', () => {
+  notifier.notify({
+    title: 'Pushbullet service stopped',
+    message: 'The pushbullet notification service has stopped. Please restart it if you wish to receive notifications from other pushbullet devices.'
+  });
 });
